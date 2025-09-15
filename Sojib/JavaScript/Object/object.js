64f94ba58986206d86687157f2b03f199692df3e
SojibIU <mdsojibhosen444@gmail.com>
2025-09-15 18:09:02 +0600

    File Reorganised

---
+++ Sojib/JavaScript/Object/object.js
@@ -0,0 +1,16 @@
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+
+const myFather = new Person("John", "Doe", 50, "blue");
+const myMother = new Person("Sally", "Rally", 48, "green");
+
+console.log("My father is " + myFather.firstName);
+console.log("My mother is " + myMother.firstName);
+
+
+
+
