b3629c53d48cd1d1128d98292dfa67ff070b1eec
Zahangir-sumit <zahangirsumitseu@gmail.com>
2025-09-15 06:17:08 -0700

    Object.Create

--- Jahangir/Javascript/Object/Object.js
+++ Jahangir/Javascript/Object/Object.js
@@ -1,3 +1,5 @@
+// Object
+
 const person1 = {
     firstName: "Jahan",
     lastName: "Sumit",
@@ -7,3 +9,13 @@ const person1 = {
 
 console.log("Name: " + person1.fullName());
 console.log("Age: " + person1.age);
+
+
+// Object.create()
+
+const person2 = Object.create(person1);
+person2.firstName = "Nur";
+person2.lastName = "Nabi";
+person2.age = 58;
+
+console.log("Fater Name: " + person2.fullName());
